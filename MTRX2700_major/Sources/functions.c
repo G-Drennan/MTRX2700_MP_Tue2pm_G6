17547a9d2c55ebb7e9a9a89f21ff0ba0fb3c1a24
jackl39 <89060650+jackl39@users.noreply.github.com>
2022-05-11 12:59:37 +1000

    updated to include range and avg calcs

--- MTRX2700_major/Sources/functions.c
+++ MTRX2700_major/Sources/functions.c
@@ -3,15 +3,34 @@
 unsigned long handleLaserValues(unsigned long laserValue, unsigned long *laserArr) {
 
   int arrLen = 10;
-  int i, avg;
+  int i, avg, min, max, range;
   for (i = 0; i < arrLen; i++) {
     laserArr[i] = laserArr[i+1];
   }
   
   laserArr[arrLen-1] = laserValue;
   
-  avg = 0;
-  
-  return avg;  
+  max = 0;
+  min = 10000;
+
+  for (i = 0; i < arrLen; i++) {
+      if (laserArr[i] > max) {
+        max = laserArr[i];
+      }
+      if (laserArr[i] < min) {
+        min = laserArr[i];
+      }
+    }
     
+    range = max - min;
+
+    if (range < 5) {
+      avg = 0;
+      for (i = 0; i < arrLen; i++) {
+        avg = avg + laserArr[i];
+      }
+      avg = avg / arrLen;
+    }
+      
+    return avg;     
 }         
