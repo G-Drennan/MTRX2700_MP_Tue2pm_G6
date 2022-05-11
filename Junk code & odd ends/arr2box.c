af526866ffe749330a957b05e7a4b4df28d8a0dc
Ro_gdre5438_bot <64570776+G-Drennan@users.noreply.github.com>
2022-05-11 20:05:32 +1000

    Add files via upload

--- Junk code & odd ends/arr2box.c
+++ Junk code & odd ends/arr2box.c
@@ -5,14 +5,15 @@
 */
 #define BUFFER 10000 
 
-char* outputInvatory(int list[3], int len);
+char* outputInvatory(int list[3], int len, char* dis);
 
 int main(){ 
     int list[3] = {6,9,3};
     int InvatoryLen = sizeof(list)/sizeof(int); 
     //printf("%lu\n", sizeof(list)/sizeof(int));
-    char *outputMessage = outputInvatory(list, InvatoryLen);   
-   printf("%s", outputMessage);   
+    char *outputMessage;
+     outputMessage = outputInvatory(list, InvatoryLen, outputMessage);    
+   printf("%s", outputMessage);    
 } 
 
 //converts num n to char c 
@@ -26,14 +27,16 @@ char* variableString( char* string, int *position, int *n){
     string[*position] = num2char(n);
     return string; 
 }  
-
-char* outputInvatory(int list[3], int InvatoryLen){ 
+char dest[BUFFER] = "\n------------\n";
+char* outputInvatory(int list[3], int InvatoryLen, char* dis){    
    
-   char dest[BUFFER] = "\n---------------------------------\n", 
-    Tempstr0[] = "item_X    X\n", Tempstr1[16];
+   //char dest[BUFFER] = "\n------------\n";  
+    char Tempstr0[] = "item_X    X\n"; 
+    char Tempstr1[16]; 
     
-    int Tempstr0len = strlen(Tempstr0), pos1 = 5, pos2 = 10;
-    char *src ;
+    int Tempstr0len = strlen(Tempstr0); 
+    int pos1 = 5; 
+    int pos2 = 10; 
     
     // list each item and its ammount
     for(int n=0; n < InvatoryLen; n++){
@@ -43,6 +46,6 @@ char* outputInvatory(int list[3], int InvatoryLen){
         strcat(dest, src2);  
     }
 
-    strcat(dest, "---------------------------------\n\n"); 
+    strcat(dest, "------------\n\n");  
     return dest;    
 } 
