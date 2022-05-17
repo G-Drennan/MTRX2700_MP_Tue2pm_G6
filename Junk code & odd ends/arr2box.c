8c31dad977e42d7b321a3ceb450836375bd7c6ae
Ro_gdre5438_bot <64570776+G-Drennan@users.noreply.github.com>
2022-05-17 14:38:27 +1000

    Update arr2box.c

--- Junk code & odd ends/arr2box.c
+++ Junk code & odd ends/arr2box.c
@@ -5,16 +5,16 @@
 */
 #define BUFFER 10000 
 
-char* outputInvatory(int list[3], int len, char* dis);
+char* outputInvatory(int list[3], int InvatoryLen); 
 
 int main(){ 
     int list[3] = {6,9,3};
     int InvatoryLen = sizeof(list)/sizeof(int); 
     //printf("%lu\n", sizeof(list)/sizeof(int));
     char *outputMessage;
-     outputMessage = outputInvatory(list, InvatoryLen, outputMessage);    
+     outputMessage = outputInvatory(list, InvatoryLen);    
    printf("%s", outputMessage);    
-} 
+}   
 
 //converts num n to char c 
 char num2char(int *n){
@@ -26,26 +26,30 @@ char num2char(int *n){
 char* variableString( char* string, int *position, int *n){
     string[*position] = num2char(n);
     return string; 
-}  
-char dest[BUFFER] = "\n------------\n";
-char* outputInvatory(int list[3], int InvatoryLen, char* dis){    
+}
+
+char dest[BUFFER];
+char* outputInvatory(int list[3], int InvatoryLen){  
    
-   //char dest[BUFFER] = "\n------------\n";  
-    char Tempstr0[] = "item_X    X\n"; 
+      
+    char Tempstr0[] = "|  item_X    X  ";
     char Tempstr1[16]; 
     
-    int Tempstr0len = strlen(Tempstr0); 
-    int pos1 = 5; 
-    int pos2 = 10; 
-    
+    int Tempstr0len = strlen(Tempstr0);
+    int pos1 = 8;
+    int pos2 = 13;  
+    char *src; 
+    int n=0; 
+    //strcat(dest, "\n---------------------------------\n");  
+   
     // list each item and its ammount
-    for(int n=0; n < InvatoryLen; n++){
+    for(n; n < InvatoryLen; n++){
 
         char *src1 = variableString(Tempstr0, &pos1, &n);
         char *src2 = variableString(src1, &pos2, &list[n]); 
         strcat(dest, src2);  
     }
 
-    strcat(dest, "------------\n\n");  
-    return dest;    
-} 
+    strcat(dest, "\n");  
+    return dest;     
+}   
