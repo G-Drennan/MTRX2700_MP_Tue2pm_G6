0e8aa5175128913a8386fd8318e0cf669a1f3ead
jackl39 <89060650+jackl39@users.noreply.github.com>
2022-05-17 16:38:18 +1000

    Update functions.h

--- MTRX2700_major/Sources/functions.h
+++ MTRX2700_major/Sources/functions.h
@@ -1,8 +1,29 @@
 
+
 #ifndef FUNCTIONS_HEADER
 #define FUNCTIONS_HEADER
 
+typedef struct {
+    char name[100];
+    int amount;
+} item;
+
+unsigned long laserValueArr[10];
+int totalNumItems = 1;
+
+// shelf parameters
+int shelf_distance = 1500;
+int back_shelf_distance = 2000;
+int box_depth = 100;
+int item_address = 1;
+
+// state parameters
+int prev_state = 0; // start on a gap state
+int current_state;
+
 unsigned long handleLaserValues(unsigned long laserValue, unsigned long *laserArr);
+void dermineOccurence(int current_state, item** itemArray);
+
 
 #endif
-                           
+                                        
