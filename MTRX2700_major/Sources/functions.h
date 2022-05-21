f88ec9d5a8944e89eb2f105fffe0d7fba523c84a
amat7180 <101961565+amat7180@users.noreply.github.com>
2022-05-21 14:42:21 +1000

    Update functions.h

--- MTRX2700_major/Sources/functions.h
+++ MTRX2700_major/Sources/functions.h
@@ -10,17 +10,10 @@ typedef struct {
     int amount;
 } item;
 
-<<<<<<< HEAD
-#define shelf_distance 1400;
-#define back_shelf_distance 1900;
+#define shelf_distance 1000;
+#define back_shelf_distance 1500;
 #define offset 50;
 #define box_depth 100;
-=======
-#define shelf_distance 1500;
-#define back_shelf_distance 2000;
-#define offset 40;
-#define box_depth 80;
->>>>>>> a13ef62e23240a4bdf095cd01abea128a21e2651
 #define error_range 30;
 
 extern volatile int item_address;
@@ -28,8 +21,7 @@ extern volatile int prev_state;
 extern volatile int current_state;
 
 unsigned long handleLaserValues(unsigned long laserValue, unsigned long *laserArr);
-void determineOccurence(int current_state, int prev_state, item** itemArray, item* current_item);
+void determineOccurence(item** itemArray, item* current_item);
 void initialiseInventoryContents(item** itemArray);
 
 #endif
-                           
