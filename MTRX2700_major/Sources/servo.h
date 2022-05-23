77f3d714fab46b4d87b1be81fe4bc627000d2b17
jackl39 <89060650+jackl39@users.noreply.github.com>
2022-05-23 18:37:06 +1000

    Update servo.h

--- MTRX2700_major/Sources/servo.h
+++ MTRX2700_major/Sources/servo.h
@@ -1,6 +1,16 @@
 #ifndef SERVO_H
 #define SERVO_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define PI 3.14159265
+#define HEIGHT_OF_SHELF 40
+#define HEIGHT_OF_RADAR 20
+#define SHELF_DIST 150
+
 int angle_2_elivation_duty (int angle);
 
 int horizontal_distance(int angle, int lidar_dist);
